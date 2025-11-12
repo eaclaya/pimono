6a07992be4a7e01da0a9fb1f8b67134bc4e35b80
Erick Zelaya <ealexander.zm@gmail.com>
2025-11-12 18:37:06 +0100

    Create Exception classes for specific errors

---
+++ api/app/Exceptions/InsufficientBalanceException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InsufficientBalanceException extends Exception
+{
+    protected $message = 'Insufficient balance for this transaction';
+    protected $code = 422;
+
+    public function __construct(float $required, float $available)
+    {
+        parent::__construct(
+            "Insufficient balance. Required: {$required}, Available: {$available}"
+        );
+    }
+
+    public function render($request)
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'error' => 'insufficient_balance'
+        ], 422);
+    }
+}
---
+++ api/app/Exceptions/TransactionFailedException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class TransactionFailedException extends Exception
+{
+    protected $code = 500;
+
+    public function render($request)
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'error' => 'transaction_failed'
+        ], 500);
+    }
+}
