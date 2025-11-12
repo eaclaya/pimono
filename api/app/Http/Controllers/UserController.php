1d4b8b75d7415ed957864af138126758085ff135
Erick Zelaya <ealexander.zm@gmail.com>
2025-11-12 18:44:06 +0100

    Create user controller to manage profile and receivers endpoints

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -8,9 +8,40 @@ use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    public function recipients()
+    public function profile()
     {
-        $users = User::whereNot('id', auth()->id())->get();
+        return new UserResource(auth()->user());
+    }
+
+    public function receivers(Request $request)
+    {
+        $query = User::whereNot('id', auth()->id());
+
+        if ($request->filled('q')) {
+            $search = $request->input('q');
+            $query->where(function ($builder) use ($search) {
+                $builder
+                    ->where('name', 'like', "%{$search}%")
+                    ->orWhere('email', 'like', "%{$search}%");
+            });
+        }
+
+        $users = $query
+            ->orderBy('name', 'asc')
+            ->paginate(20);
+
+        return UserResource::collection($users);
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+
+        $users = User::where('name', 'like', "%{$search}%")
+            ->orWhere('email', 'like', "%{$search}%")
+            ->whereNot('id', auth()->id())
+            ->orderBy('name', 'asc')
+            ->paginate(20);
 
         return UserResource::collection($users);
     }
